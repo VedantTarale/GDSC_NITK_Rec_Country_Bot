5dcf86afd6ac1ddd68f012d445cb5576a3cea720
Vedant Tarale <vedanttarale10@gmail.com>
2022-11-01 18:14:49 +0530

    minor bugfixes

--- countryBot.py
+++ countryBot.py
@@ -1,4 +1,5 @@
 # library to handle HTTP requests made to the API
+from tabnanny import check
 import requests
 
 # telegram bot labrary
@@ -7,6 +8,7 @@ from telegram.ext import *
 
 #defining a global variable to store the data so that it can be accessed by all functions.
 global data
+check = 0
 
 # creating the bot object with the unique token generated from BotFather
 updater = Updater(
@@ -16,12 +18,16 @@ dispatcher = updater.dispatcher
 
 # start command  /start
 def start(update, context):
+    global check
+    check = 0
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="Welcome To CountryBot!\nHere's a list of commands: \n/help\n/hello\n/info")
 
 
 # hello command /hello
 def hello(update, context):
+    global check
+    check = 0
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="Hello! Great to see you!")
 
@@ -29,6 +35,7 @@ def hello(update, context):
 # info command  /info <country_name>
 def info(update, context):
     global data
+    global check
     # user_params stores the <country_name> sent with /info <country_name> command
     user_params = " ".join(context.args)
 
@@ -45,6 +52,7 @@ def info(update, context):
 
             # storing the recieved json response
             data = response.json()[0]
+            check = 1
             context.bot.send_message(
                 chat_id=update.effective_chat.id, text="Please select an option",reply_markup=ReplyKeyboardMarkup(buttons))
         # If response was not generated successfully
@@ -61,62 +69,68 @@ def info(update, context):
 # Function to handle text-inputs for selectively displaying the deatils of the country
 def options_handler(update, context):
     global data
-    if "All Details" in update.message.text:
-        currency_data = list(data['currencies'].values())
-        languages = ','.join(list(data['languages'].values()))
-        # Creating a string to store the formatted response
-        reply_text = "Official Name: "+str(data['name']['official'])+"\n"
-        reply_text += "Capital: "+str(data['capital'][0])+"\n"
-        reply_text += "Languages Spoken: "+languages+"\n"
-        reply_text += "Currency: " + \
-            str(currency_data[0]['name']) + \
-            "("+str(currency_data[0]['symbol'])+")\n"
-        reply_text += "Area: "+str(data['area'])+" km^2\n"
-        reply_text += "Population: "+str(data['population'])+"\n"
-        reply_text += "Continent: " + \
-            str(data['continents'][0])+"\n"
-        reply_text += "Flag: "+str(data['flags']['png'])+"\n"
-        reply_text += "Map: "+str(data['maps']['googleMaps'])
-        # Sending the data to the user
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text=reply_text)
-    elif "Official Name" in update.message.text:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Official Name: "+str(data['name']['official'])+"\n")
-    elif "Capital" in update.message.text:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Capital: "+str(data['capital'][0])+"\n")
-    elif "Languages Spoken" in update.message.text:
-        languages = ','.join(list(data['languages'].values()))
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Languages Spoken: "+languages+"\n")
-    elif "Currency" in update.message.text:
-        currency_data = list(data['currencies'].values())
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Currency: " +
-                                 str(currency_data[0]['name']) +
-                                 "("+str(currency_data[0]['symbol'])+")\n")
-    elif "Area" in update.message.text:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Area: "+str(data['area'])+" km^2\n")
-    elif "Population" in update.message.text:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Population: "+str(data['population'])+"\n")
-    elif "Continent" in update.message.text:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Continent: " + str(data['continents'][0])+"\n")
-    elif "Flag" in update.message.text:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Flag: "+str(data['flags']['png'])+"\n")
-    elif "Map" in update.message.text:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Map: "+str(data['maps']['googleMaps']))
+    global check
+    if check:
+        if "All Details" in update.message.text:
+            currency_data = list(data['currencies'].values())
+            languages = ','.join(list(data['languages'].values()))
+            # Creating a string to store the formatted response
+            reply_text = "Official Name: "+str(data['name']['official'])+"\n"
+            reply_text += "Capital: "+str(data['capital'][0])+"\n"
+            reply_text += "Languages Spoken: "+languages+"\n"
+            reply_text += "Currency: " + \
+                str(currency_data[0]['name']) + \
+                "("+str(currency_data[0]['symbol'])+")\n"
+            reply_text += "Area: "+str(data['area'])+" km^2\n"
+            reply_text += "Population: "+str(data['population'])+"\n"
+            reply_text += "Continent: " + \
+                str(data['continents'][0])+"\n"
+            reply_text += "Flag: "+str(data['flags']['png'])+"\n"
+            reply_text += "Map: "+str(data['maps']['googleMaps'])
+            # Sending the data to the user
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text=reply_text)
+        elif "Official Name" in update.message.text:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                    text="Official Name: "+str(data['name']['official'])+"\n")
+        elif "Capital" in update.message.text:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Capital: "+str(data['capital'][0])+"\n")
+        elif "Languages Spoken" in update.message.text:
+            languages = ','.join(list(data['languages'].values()))
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Languages Spoken: "+languages+"\n")
+        elif "Currency" in update.message.text:
+            currency_data = list(data['currencies'].values())
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Currency: " +
+                                    str(currency_data[0]['name']) +
+                                    "("+str(currency_data[0]['symbol'])+")\n")
+        elif "Area" in update.message.text:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Area: "+str(data['area'])+" km^2\n")
+        elif "Population" in update.message.text:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Population: "+str(data['population'])+"\n")
+        elif "Continent" in update.message.text:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Continent: " + str(data['continents'][0])+"\n")
+        elif "Flag" in update.message.text:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Flag: "+str(data['flags']['png'])+"\n")
+        elif "Map" in update.message.text:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Map: "+str(data['maps']['googleMaps']))
+        else:
+            context.bot.send_message(
+                chat_id=update.effective_chat.id, text="Sorry, entered text is invalid.")
     else:
         context.bot.send_message(
-            chat_id=update.effective_chat.id, text="Sorry, entered text is invalid.")
-
+                chat_id=update.effective_chat.id, text="Please use /info <country_name>")
 
 #Help function to display list of available commands    /help
 def help(update, context):
+    global check
+    check = 0
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Here are the currently defined commands: \n/start\n/hello\n/info")
 
